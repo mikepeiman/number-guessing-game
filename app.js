8bb00dcf3d9dac2c89d4d6153bc2f3e0ef987270
Mike Peiman <mikepeiman@gmail.com>
2018-06-14 21:35:21 -0700

    Added error message function and win case

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ Game function:
 */
 
 // Game values
-let min = 33,
-    max = 77,
+let min = 1,
+    max = 10,
     winNum = 7,
     guessesRemaining = 3
 
@@ -24,3 +24,36 @@ const UIgame = document.querySelector('#game'),
 // assign UI min and max
 UImin.textContent = min;
 UImax.textContent = max;
+
+// Listen for guess
+UIguessBtn.addEventListener('click', function() {
+  let guess = parseInt(UIguessInput.value)
+  
+
+  // validate guess (between min-max range)
+  if(isNaN(guess) || guess < min || guess > max) {
+    console.log('Not a valid guess. Guess within range')
+    setMessage(`Please enter a number between ${min} and ${max}`, 'red')
+  } else {
+    
+  }
+
+  // check if won
+  if(guess === winNum) {
+  // disable input
+  UIguessInput.disabled = true
+  // inform player of win
+    setMessage(`${winNum} is correct! Good guess - you win! :)`, 'green')
+  // make border green
+  UIguessInput.style.borderColor = 'green'
+  // change submit button to offer "play again"
+  } else {
+
+  }
+})
+
+// set error message function
+function setMessage(msg, color) {
+  UImessage.style.color = color
+  UImessage.textContent = msg
+}
