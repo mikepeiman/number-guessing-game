8a927934a0f94cbdd251e8e3bd4919f06ab2842a
Mike Peiman <mikepeiman@gmail.com>
2018-06-15 09:28:13 -0700

    refactored processGuess function, added keyup===13 (enter) event listener, created the 'play again' functionality, created generateRandomNumber function for winning number.

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ Game function:
 */
 
 // Game values
-let min = 1,
-    max = 10,
-    winNum = 7,
+let min = 30,
+    max = 50,
+    winNum = generateRandomNumber(min, max),
     guessesRemaining = 3
 
 // UI elements
@@ -26,46 +26,52 @@ const UIgame = document.querySelector('#game'),
 UImin.textContent = min;
 UImax.textContent = max;
 
-// Listen for guess
-UIguessBtn.addEventListener('click', function() {
-  let guess = parseInt(UIguessInput.value)
-  // guessesRemainingMessage(`You have ${guessesRemaining-1} guesses left.`, 'grey')
-
-
-  
-
-  // validate guess (between min-max range)
-  if(isNaN(guess) || guess < min || guess > max) {
-    console.log('Not a valid guess. Guess within range')
-    setMessage(`Please enter a number between ${min} and ${max}`, 'red')
-  } else {
-    
+// Play-again event listener
+UIgame.addEventListener('mousedown', function(e) {
+  if(e.target.className === 'play-again') {
+    window.location.reload()
   }
+  console.log('1')
+})
 
-  // check if won
-  if(guess === winNum) {
-  // disable input
-  UIguessInput.disabled = true
-  // inform player of win
-  gameOver(true, `${winNum} is correct! Good guess - you win! :)`)
-  // change submit button to offer "play again"
-  
-  } else {
-    //  guess incorrect
-    guessesRemaining -= 1
-    // check if game continues
-    if(guessesRemaining === 0) {
-      gameOver(false, `Awww, you lost! The winning number was ${winNum}`)
-    } else {
-      // Game continues, guess incorrect
-      // border color
-      UIguessInput.style.borderColor = 'red'
-      UIguessInput.value = ''
-      UIguessInput.focus()
-      setMessage(`${guess} is incorrect, ${guessesRemaining} guesses left, try again`, 'red')
-    }
+
+UIguessInput.addEventListener('keyup', function(e) {
+  if(e.keyCode === 13) {
+    processGuess()
   }
 })
+UIguessBtn.addEventListener('click', processGuess)
+
+function processGuess(e) {
+  let guess = parseInt(UIguessInput.value)
+      // validate guess (between min-max range)
+      if(isNaN(guess) || guess < min || guess > max) {
+        console.log('Not a valid guess. Guess within range')
+        setMessage(`Please enter a number between ${min} and ${max}`, 'red')
+      } 
+    
+      // check if won
+      if(guess === winNum) {
+        // disable input
+        UIguessInput.disabled = true
+        // inform player of win
+        gameOver(true, `${winNum} is correct! Good guess - you win! :)`)
+      } else {
+        //  guess incorrect
+        guessesRemaining -= 1
+        // check if game continues
+        if(guessesRemaining === 0) {
+          gameOver(false, `Awww, you lost! The winning number was ${winNum}`)
+        } else {
+          // Game continues, guess incorrect
+          // border color
+          UIguessInput.style.borderColor = 'red'
+          UIguessInput.value = ''
+          UIguessInput.focus()
+          setMessage(`${guess} is incorrect, ${guessesRemaining} guesses left, try again`, 'red')
+        }
+      }
+}
 
 // set error message function
 function setMessage(msg, color) {
@@ -76,6 +82,9 @@ function setMessage(msg, color) {
 //   UIguessesRemaining.style.color = color
 //   UIguessesRemaining.textContent = msg
 // }
+function generateRandomNumber(min, max) {
+  return Math.floor(Math.random()*(max-min+1)+min)
+}
 
 function gameOver(won, msg) {
   let color
@@ -85,5 +94,8 @@ function gameOver(won, msg) {
   UIguessInput.style.borderColor = color
   UImessage.style.color = color
   setMessage(msg, color)
+  UIguessBtn.value = 'Play again'
+  UIguessBtn.className += 'play-again'
+  guessesRemaining = 3
 
 }
